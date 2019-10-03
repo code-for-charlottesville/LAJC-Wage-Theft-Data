a728b401fe6f77994380767a3802c8aaeccdd568
dgoldstein1 <david.goldstein@deciphernow.com>
2019-10-02 22:01:54 -0400

    polished up docs

--- elastic-kibana/README.md
+++ elastic-kibana/README.md
@@ -1,1 +1,51 @@
-TODO
+# Elastic-Kibana Setup
+
+A Python simple python script and development environment to index LAJC data to an elastic search cluster.
+
+![dashboard](./imgs/dashboard.png)
+
+See Links for:
+
+- [kibana dashboard on AWS](https://bit.ly/2oCCaAw)
+- [elastic cluster on AWS]( https://search-lajc-wage-theft-oiaka2yn2mw4onwkkslm4oy4pa.us-east-1.es.amazonaws.com)
+
+## Setup and Run
+
+Requirements:
+
+- [python 2](https://www.python.org/downloads/release/python-2716/)
+- [pip](https://pip.pypa.io/en/stable/installing/)
+- [docker](https://docs.docker.com/install/linux/docker-ce/ubuntu/)
+
+Steps:
+
+```bash
+# install requirements
+$ cd elastic-kibana
+$ pip install -r requirements.txt
+..
+# make sure the script compiles
+$ python index_data.py --help
+
+Indexes csv data from '../Raw data/lajc_clean.csv' into an elastic 
+search cluster under the index 'claims'
+
+USAGE: 
+	python2.7 index_data.py [ELASTIC_SEARCH_ENDPOINT or --help]
+# start local instances of elastic and kibana
+$ docker-compose up -d
+...
+# index data into local elastic search endpoint
+$ python index_data.py http://localhost:9200
+..
+indexing claim 3984 of 3948
+# see that data was succesfully loaded
+$ curl http://localhost:9200/claims/search | jq
+...
+```
+
+## Authors
+
+- Dan Harris [dshva](https://github.com/dshva)
+- David Goldstein [dgoldstein1](https://github.com/dgoldstein1)
+
--- elastic-kibana/index_data.py
+++ elastic-kibana/index_data.py
@@ -1,24 +1,42 @@
 # !/bin/python
-# pip install elasticsearch
+
 from elasticsearch import Elasticsearch
 import pandas as pd
 import json
-import uuid
+import logging
+import sys
 
-ES_ENDPOINT = 'https://search-lajc-wage-theft-oiaka2yn2mw4onwkkslm4oy4pa.us-east-1.es.amazonaws.com/'
 DATA_INDEX = 'claims'
-DATA_FILE= '../Raw data/lajc_clean.csv'
+DATA_FILE = '../Raw data/lajc_clean.csv'
 
-es = Elasticsearch([ES_ENDPOINT])
+USAGE = """
+Indexes csv data from '{}' into an elastic 
+search cluster under the index '{}'
+
+USAGE: 
+	python2.7 index_data.py [ELASTIC_SEARCH_ENDPOINT or --help]
+""".format(DATA_FILE, DATA_INDEX)
 
 if __name__ == '__main__':
-  claims_df = pd.read_csv(DATA_FILE)
-  docs = json.loads(claims_df.to_json(orient='records'))
-  for doc in docs:
-    doc_id = str(uuid.uuid4())
-    es.index(index=DATA_INDEX, doc_type='_doc', id=doc_id, body=json.dumps(doc))
-
-# https://search-lajc-wage-theft-oiaka2yn2mw4onwkkslm4oy4pa.us-east-1.es.amazonaws.com/_plugin/kibana/app/kibana#/dashboard/08157150-e578-11e9-8e9b-cf6e52d7d1e7?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-4y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:'1',w:24,x:0,y:0),id:'6e03a350-e574-11e9-8e9b-cf6e52d7d1e7',panelIndex:'1',type:visualization,version:'7.1.1'),(embeddableConfig:(),gridData:(h:15,i:'2',w:24,x:24,y:0),id:'3b55a140-e576-11e9-8e9b-cf6e52d7d1e7',panelIndex:'2',type:visualization,version:'7.1.1'),(embeddableConfig:(),gridData:(h:15,i:'3',w:24,x:0,y:15),id:'5513ca20-e577-11e9-8e9b-cf6e52d7d1e7',panelIndex:'3',type:visualization,version:'7.1.1'),(embeddableConfig:(),gridData:(h:15,i:'4',w:24,x:24,y:15),id:f4cd8790-e577-11e9-8e9b-cf6e52d7d1e7,panelIndex:'4',type:visualization,version:'7.1.1')),query:(language:kuery,query:''),timeRestore:!f,title:'New%20Dashboard',viewMode:view)
-
-# es: https://search-lajc-wage-theft-oiaka2yn2mw4onwkkslm4oy4pa.us-east-1.es.amazonaws.com
-# kibana: https://search-lajc-wage-theft-oiaka2yn2mw4onwkkslm4oy4pa.us-east-1.es.amazonaws.com/_plugin/kibana/
+    # parse args
+    if len(sys.argv) != 2 or sys.argv[1] == "--help":
+        print USAGE
+        sys.exit(1)
+    # init data file
+    logging.basicConfig(filename="index.log", level=logging.DEBUG)
+    logging.info("Reading in datafile: %s", DATA_FILE)
+    claims_df = pd.read_csv(DATA_FILE)
+    docs = json.loads(claims_df.to_json(orient='records'))
+    esEndpoint = sys.argv[1]
+    es = Elasticsearch([esEndpoint])
+    logging.info("indexing to endpoint %s", esEndpoint)
+    i = 0
+    for doc in docs:
+        doc_id = str(doc["claimID"])
+        logging.info("indexing claimID: %s", doc["claimID"])
+        i += 1
+        print "indexing claim {} of {}".format(i, len(docs))
+        es.index(index=DATA_INDEX,
+                 doc_type='_doc',
+                 id=doc_id,
+                 body=json.dumps(doc))
